b4441fab854633775910004a1abc80afbd34fdf2
somsom <som.jaiswal.sj@gmail.com>
2020-09-27 15:57:28 +0530

    Adding site

--- AppendMethod/style.js
+++ AppendMethod/style.js
@@ -3,8 +3,19 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 button.addEventListener("click",function(){
-  console.log(input.value);
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
+  if(input.value.length > 0){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+  }
+})
+
+input.addEventListener("keypress",function(event){
+  if(input.value.length > 0&& event.which === 13){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+  }
 })
